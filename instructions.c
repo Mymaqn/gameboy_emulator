3b40d0036a3c2564498acd00210bb1f3b25eab94
Zopazz <jensfox999@live.dk>
2020-10-26 23:58:37 +0200

    Created and tested adc in instructions.c. Created a utility function for checking the arithmetic target

--- instructions.c
+++ instructions.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include "registers.c"
 #include <limits.h>
+#include <stdbool.h>
 
 
 enum ArithmeticTarget{A = 0,B,C,D,E,H,L};
-//Adding on a gameboy works by giving it a target register and adding it to the a register.
-void instructions_add(Reg* reg,enum ArithmeticTarget target){
-
+//Utility function to avoid code repetition
+unsigned char instructions_getTargetValue(Reg* reg, enum ArithmeticTarget target){
   unsigned char value;
-
   switch (target) {
     case 0:
       value = reg->a;
@@ -40,8 +39,15 @@ void instructions_add(Reg* reg,enum ArithmeticTarget target){
 
     default:
       printf("Invalid register, number passed was: %d",target);
-      return;
+      return 0;
   }
+  return value;
+}
+
+//Adding on a gameboy works by giving it a target register and adding it to the a register.
+void instructions_add(Reg* reg,enum ArithmeticTarget target){
+
+  unsigned char value = instructions_getTargetValue(reg,target);
 
   //Grab the current flag register
   FlagsRegister flags = registers_RegfToFlagsRegister(reg->f);
@@ -50,9 +56,15 @@ void instructions_add(Reg* reg,enum ArithmeticTarget target){
   if (value > (UCHAR_MAX - reg->a)) {
     flags.carry = true;
   }
+  else{
+    flags.carry = false;
+  }
   //Check for half-carry/half-overflow
   if( (reg->a & 0xF) + (value&0xF) > 0xF){
     flags.half_carry = true;
+  }
+  else{
+    flags.half_carry = false;
   }
 
   reg->a = reg->a+value;
@@ -60,6 +72,9 @@ void instructions_add(Reg* reg,enum ArithmeticTarget target){
   //Check if the output is 0
   if(reg->a == 0){
     flags.zero = true;
+  }
+  else{
+    flags.zero = false;
   }
 
   flags.subtract = false;
@@ -70,6 +85,50 @@ void instructions_add(Reg* reg,enum ArithmeticTarget target){
   return;
 }
 
+void instructions_adc(Reg* reg, enum ArithmeticTarget target){
+  unsigned char value = instructions_getTargetValue(reg,target);
+
+  //Grab the current flag register
+  FlagsRegister flags = registers_RegfToFlagsRegister(reg->f);
+
+  bool carrySet = flags.carry;
+
+  //Check for carry/overflow. This needs to be done before adding.
+  if (value > (UCHAR_MAX - (reg->a+carrySet))) {
+    flags.carry = true;
+  }
+  else{
+    flags.carry = false;
+  }
+  //Check for half-carry/half-overflow
+  if( ((reg->a & 0xF) + (value & 0xF))+carrySet > 0xF){
+    flags.half_carry = true;
+  }
+  else{
+    flags.half_carry = false;
+  }
+
+  reg->a = reg->a+value + carrySet;
+
+  //Check if the output is 0
+  if(reg->a == 0){
+    flags.zero = true;
+  }
+  else{
+    flags.zero = false;
+  }
+
+  flags.subtract = false;
+
+  //Set the f register
+  reg->f = registers_FlagsRegisterToRegf(flags);
+
+  return;
+
+}
+
+
+
 
 int main(void){
 
@@ -79,6 +138,14 @@ int main(void){
 
   instructions_add(&reg1,C);
 
-  printf("%d\n",reg1.a);
-  printf("%d",reg1.f);
+  printf("Expecting f to be 48 and a to be 4: \nf:%d\na:%d",reg1.f,reg1.a);
+
+  instructions_adc(&reg1,C);
+  reg1.c = 220;
+
+  printf("\nExpecting f to be 32 and a to be 225\nf:%d\na:%d",reg1.f,reg1.a);
+  FlagsRegister flags = registers_RegfToFlagsRegister(32);
+  printf("Flags: %d, %d, %d, %d",flags.zero,flags.carry,flags.half_carry,flags.subtract);
+
+
 }
